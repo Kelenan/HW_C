ee8dd25e23dd5def909ee94e2f1f6dac67cb31b1
«Elena <leseee4ka@bk.ru>
2023-07-22 13:38:55 +0300

    exe2

---
+++ exe2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2
+Console.WriteLine("Введите первое число");
+string number1 = Console.ReadLine() ?? "";
+int a = int.Parse(number1);
+Console.WriteLine("Введите второе число");
+string number2 = Console.ReadLine() ?? "";
+int b = int.Parse(number2);
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else 
+{
+    Console.WriteLine($"max = {b}");
+}
